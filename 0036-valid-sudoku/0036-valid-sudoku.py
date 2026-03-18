0d6fff4a4762690b0814e9a84d87081aa7478281
Anay Pradhan <115114923+anay0304@users.noreply.github.com>
2026-03-18 06:29:28 -0700

    Time: 9 ms (34.73%), Space: 12.2 MB (99.87%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def isValidSudoku(self, board):
+        """
+        :type board: List[List[str]]
+        :rtype: bool
+        """
+
+        rows = collections.defaultdict(set) #Hash set
+        cols = collections.defaultdict(set)
+        box = collections.defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == '.':
+                    continue
+                
+                if (board[r][c] in rows[r] or 
+                    board[r][c] in cols[c] or
+                    board[r][c] in box[r // 3, c // 3]):
+                    return False
+
+                rows[r].add(board[r][c])
+                cols[c].add(board[r][c])
+                box[r // 3, c// 3].add(board[r][c])
+        
+        return True
+
+
+
+
+        
