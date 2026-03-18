19b5dc488e70930110aceffd8fd73db9eee30a17
Anay Pradhan <115114923+anay0304@users.noreply.github.com>
2026-03-18 05:44:47 -0700

    Time: 6 ms (87.58%), Space: 14.4 MB (54.51%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+
+        map = {}
+        result = []
+
+        for i in nums:
+            map[i] = map.get(i, 0) + 1
+
+        sortedMap = sorted(map.items(), key=lambda x: x[1], reverse=True)
+
+        for i in range(k):
+            result.append(sortedMap[i][0])
+
+        return result
