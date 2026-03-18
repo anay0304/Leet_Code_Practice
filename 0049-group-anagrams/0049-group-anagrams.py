c45cbfe0d80cfb48ceccc4dbb61ab3bf78d39d93
Anay Pradhan <115114923+anay0304@users.noreply.github.com>
2026-03-17 19:06:30 -0700

    Time: 22 ms (57.02%), Space: 16.7 MB (38.11%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
+
+        map = {}
+        
+        for i in strs:
+            key = tuple(sorted(i))
+            
+            if key not in map:
+                map[key] = []
+
+            map[key].append(i)
+
+        return map.values()
