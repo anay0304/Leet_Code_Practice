30a0f70315fa89a0ce4c03f5b827ba22e0d89fd5
Anay Pradhan <115114923+anay0304@users.noreply.github.com>
2026-03-30 07:09:43 -0700

    Time: 4 ms (78.25%), Space: 12.5 MB (42.47%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -13,18 +13,18 @@ class Solution(object):
 
             for c in range(9):
 
-                if (board[r][c] == '.'):
+                if board[r][c] == ".":
                     continue
 
-                if (board[r][c] in rows[r] or
-                    board[r][c] in cols[c] or
-                    board[r][c] in box[r // 3, c // 3]):
+                if (
+                    (board[r][c] in rows[r]) or
+                    (board[r][c] in cols[c]) or
+                    (board[r][c] in box[r // 3, c // 3])
+                    ):
+
                     return False
 
-                
                 rows[r].add(board[r][c])
                 cols[c].add(board[r][c])
                 box[r // 3, c // 3].add(board[r][c])
-
         return True
-                
